a333fc2a9276978707fa25924b0be502616251bc
Raspberry Pi <raspberrypisig@gmail.com>
2024-12-13 19:09:50 +1100

    hack

--- src/build123d_poly/drawing_1d.py
+++ src/build123d_poly/drawing_1d.py
@@ -1,18 +1,5 @@
 # Automatically generated functions from generate_drawing_1d.py
-
 from build123d import *
 
-"""
-
-
-def tangent_arc(end_point: Union, tangent: Union, tangent_from_first: bool = True, mode: Mode = <Mode.ADD>) -> TangentArc:
+def tangent_arc(end_point: VectorLike, tangent: VectorLike, tangent_from_first: bool = True,mode: Mode = Mode.ADD) -> TangentArc:
     pass
-
-
-
-"""
-
-
-def tangent_arc( end_point: VectorLike, tangent: VectorLike, tangent_from_first: bool = True,mode: Mode = Mode.ADD  )-> TangentArc:
-    pass
-
